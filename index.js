2aa1622ccd678db302a59f8bf3fc85072cf982f6
Tiago Garcia <tiago.garcia@macys.com>
2017-05-02 00:19:23 -0700

    all working

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var Metalsmith      = require('metalsmith');
 var collections     = require('metalsmith-collections');
-var layouts         = require('metalsmith-layouts');
+var templates         = require('metalsmith-templates');
 var markdown        = require('metalsmith-markdown');
 var permalinks      = require('metalsmith-permalinks');
-var googleAnalytics = require('metalsmith-google-analytics').default;
 var serve           = require('metalsmith-serve');
 var watch           = require('metalsmith-watch');
 
@@ -18,20 +17,20 @@ Metalsmith(__dirname)         // __dirname defined by node.js:
   .source('./source/_posts')            // source directory
   .destination('./public')     // destination directory
   .clean(true)                // clean destination before
-  .use(collections({          // group all blog posts by internally
-    posts: 'source/_posts/*.md',
-    sortBy: 'date',
-    reverse: true
-  }))                         // use `collections.posts` in layouts
+  .use(collections({
+    articles: {
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
   .use(markdown())            // transpile all md into html
   .use(permalinks({           // change URLs to permalink URLs
     relative: false           // put css only in /css
   }))
-  .use(layouts({              // wrap layouts around html
+  .use(templates({              // wrap layouts around html
     engine: 'handlebars',     // use the layout engine you like
     directory: 'source/_layouts'
   }))
-  //.use(googleAnalytics('UA-98418104-1'))
   .use(serve({
     port: 8080,
     verbose: true
--- source/_posts/index.md
+++ source/_posts/index.md
@@ -1,4 +1,3 @@
 ---
 template: default.html
 ---
-INDEX
